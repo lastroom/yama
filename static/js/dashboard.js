b099954d6dace1e4518845941c2f3db27a178c74
Julian Ceballos <julian@pikhub.com>
2014-06-05 16:55:27 -0500

    cmd assistant, pretty code

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
     
     $('input[type="checkbox"]').on('click', function() {
         var name = $(this).attr('id');
-        console.log('input[name="' + name + '"]');
         $('input[name="' + name + '"]').val(this.checked);
     });
 
