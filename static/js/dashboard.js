1b403f91df2f4ff852214183acfd711e65bed4a4
Etienne <evoilliot@gmail.com>
2014-09-23 09:39:18 +0200

    Added : more possibilities for the select widget datasource, enhanced management of field display

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    
+
     $('input[type="checkbox"]').on('click', function() {
         var name = $(this).attr('id');
         $('input[name="' + name + '"]').val(this.checked);
@@ -7,13 +7,13 @@ $(document).ready(function() {
 
     $('.tagit').each(function() {
         var fieldOptions = $(this).data();
-        
+
         var options = {
             fieldName: fieldOptions.fieldName,
             placeholderText: fieldOptions.placeholderText || "Type here...",
             beforeTagAdded: function(event, ui) {
                 if (fieldOptions.pattern != "") {
-                    var re  = new RegExp(fieldOptions.pattern);
+                    var re = new RegExp(fieldOptions.pattern);
                     if (!re.test(ui.tagLabel)) {
                         $(this).attr('style', 'border: 1px red solid');
                         return false;
